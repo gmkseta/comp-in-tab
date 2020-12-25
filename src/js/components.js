a97cad63f9c01748e915118fc96a03278ba2a7b1
gmkseta <gmkseta@gmail.com>
2020-12-25 21:23:44 +0900

    for issue

---
+++ src/js/components.js
@@ -0,0 +1,5 @@
+import Framework7 from "framework7/bundle";
+import CustomComponent from "../pages/custom_comp.f7.html"
+
+
+Framework7.registerComponent("custom-comp", CustomComponent);
---
+++ src/js/routes.js
@@ -0,0 +1,15 @@
+
+import HomePage from '../pages/home.f7.html';
+import TabPage from "../pages/tabs.f7.html";
+var routes = [
+  {
+    path: '/',
+    component: HomePage,
+  },
+  {
+    path: '/tabs',
+    component: TabPage,
+  }
+];
+
+export default routes;
